67c4506d5822169b3b02d9a4c7805883e8bd80af
pejuangvscode <151711997+pejuangvscode@users.noreply.github.com>
2025-06-19 20:59:56 +0700

    fix: tailwind error

--- next.config.js
+++ next.config.js
@@ -7,9 +7,6 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  eslint: {
-        ignoreDuringBuilds: true,
-    },
 
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
